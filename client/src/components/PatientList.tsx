c5e7f433e8527f863047d028695302c7fd685f62
Erol <erol.a.cimen@gmail.com>
2020-12-12 15:12:39 +1100

    added conditional rendering

--- client/src/components/PatientList.tsx
+++ client/src/components/PatientList.tsx
@@ -7,24 +7,32 @@ interface Props {
 }
 
 export const PatientList: React.FC<Props> = ({ patients }) => {
-    return (
-        <table className="patient-container">
-            <tbody>
-            <tr id = "table-headers">
-                <th id = "bed-number-header">Bed Number</th>
-                <th id = "test-info-last-header">
-                   Last Screen Performed
-                </th> 
-                <th id = "test-info-next-header">
-                    Next Screen Due
-                </th>      
-            </tr>
-            {
-                patients.map((pt, index) => (
-                    <PatientEntry key = {index} {...pt} />
-                ))
-            }
-            </tbody>
-        </table>
-    )
+    if (patients.length == 0)  {
+        return (
+            <div>
+                There are no patients currently being tracked. 
+            </div>
+        )
+    } else {
+        return (
+            <table className="patient-container">
+                <tbody>
+                <tr id = "table-headers">
+                    <th id = "bed-number-header">Bed Number</th>
+                    <th id = "test-info-last-header">
+                    Last Screen Performed
+                    </th> 
+                    <th id = "test-info-next-header">
+                        Next Screen Due
+                    </th>      
+                </tr>
+                {
+                    patients.map((pt, index) => (
+                        <PatientEntry key = {index} {...pt} />
+                    ))
+                }
+                </tbody>
+            </table>
+        )
+    }
 }
