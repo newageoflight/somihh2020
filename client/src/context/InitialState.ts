a44ca4c9094e2bec6e0eb7e87cd6090e0a60a217
newageoflight <camint3rnal@live.com>
2020-12-12 21:59:41 +1100

    Moves InitialState to a separate file

---
+++ client/src/context/InitialState.ts
@@ -0,0 +1,62 @@
+export const InitialState = [
+    {
+        "bedNumber": "x9n0",
+        "lastScreenDT": "2020-12-12T11:23Z",
+        "lastScreenPassed": "notDone",
+        "nextScreenDT": "2020-12-13T11:23Z"
+    },
+    {
+        "bedNumber": "x9n1",
+        "lastScreenDT": "2020-12-11T22:54Z",
+        "lastScreenPassed": "notDone",
+        "nextScreenDT": "2020-12-12T22:54Z"
+    },
+    {
+        "bedNumber": "x9n2",
+        "lastScreenDT": "2020-12-12T07:55Z",
+        "lastScreenPassed": "passed",
+        "nextScreenDT": "2020-12-13T07:55Z"
+    },
+    {
+        "bedNumber": "x9n3",
+        "lastScreenDT": "2020-12-12T10:43Z",
+        "lastScreenPassed": "failed",
+        "nextScreenDT": "2020-12-13T10:43Z"
+    },
+    {
+        "bedNumber": "x9n4",
+        "lastScreenDT": "2020-12-12T02:13Z",
+        "lastScreenPassed": "failed",
+        "nextScreenDT": "2020-12-13T02:13Z"
+    },
+    {
+        "bedNumber": "x9n5",
+        "lastScreenDT": "2020-12-12T17:55Z",
+        "lastScreenPassed": "passed",
+        "nextScreenDT": "2020-12-13T17:55Z"
+    },
+    {
+        "bedNumber": "x9n6",
+        "lastScreenDT": "2020-12-12T12:39Z",
+        "lastScreenPassed": "failed",
+        "nextScreenDT": "2020-12-13T12:39Z"
+    },
+    {
+        "bedNumber": "x9n7",
+        "lastScreenDT": "2020-12-11T07:14Z",
+        "lastScreenPassed": "passed",
+        "nextScreenDT": "2020-12-12T07:14Z"
+    },
+    {
+        "bedNumber": "x9n8",
+        "lastScreenDT": "2020-12-11T02:54Z",
+        "lastScreenPassed": null,
+        "nextScreenDT": "2020-12-12T02:54Z"
+    },
+    {
+        "bedNumber": "x9n9",
+        "lastScreenDT": "2020-12-11T13:35Z",
+        "lastScreenPassed": null,
+        "nextScreenDT": "2020-12-12T13:35Z"
+    }
+]
