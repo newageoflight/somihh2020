e7f026ce18459f64fa3acf4b4257408edf162a52
newageoflight <camint3rnal@live.com>
2020-12-12 11:27:51 +1100

    Adds JSON preprocessor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { PatientList } from './components/PatientList';
+import { preprocessPatients, PatientInterface } from './interfaces/Patient';
 
 function App() {
-  const [appState, setAppState] = useState([]);
+  const [appState, setAppState] = useState<Array<PatientInterface>>([]);
 
   useEffect(() => {
     async function getData() {
       const getFetch = await fetch("fake-data.json");
       const getJSON = await getFetch.json();
+      const appData = preprocessPatients(getJSON);
 
-      setAppState(getJSON);
+      setAppState(appData);
     }
 
     getData();
--- client/src/interfaces/Patient.tsx
+++ client/src/interfaces/Patient.tsx
@@ -5,3 +5,26 @@ export interface PatientInterface {
     nextScreenDT: Date;
     connected: boolean;
 }
+
+interface RawPatientInterface {
+    bedNumber: string;
+    lastScreenDT: string;
+    lastScreenPassed: "passed" | "failed" | "notDone" | null;
+    nextScreenDT: string;
+    connected: boolean;
+}
+
+export function preprocessPatients(pts: Array<RawPatientInterface>): Array<PatientInterface> {
+    let newPts = Array<PatientInterface>();
+    for (let index = 0; index < pts.length; index++) {
+        const rawPt = pts[index];
+        let {lastScreenDT, nextScreenDT} = rawPt;
+        let lastScreenDate = new Date(lastScreenDT);
+        let nextScreenDate = new Date(nextScreenDT);
+        let newPt = {
+            ...rawPt, lastScreenDT: lastScreenDate, nextScreenDT: nextScreenDate
+        }
+        newPts.push(newPt);
+    }
+    return newPts;
+}
