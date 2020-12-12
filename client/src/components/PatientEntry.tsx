6c520bbd10f4768911ad87c217d5c318af47f4de
Erol <erol.a.cimen@gmail.com>
2020-12-13 00:10:09 +1100

    added alt text to the font-awesome icons

--- client/src/components/PatientEntry.tsx
+++ client/src/components/PatientEntry.tsx
@@ -28,12 +28,14 @@ export const PatientEntry: React.FC<PatientInterface> = (pt) => {
 function renderScreenResult(result: number) {
     switch (result) {
         case 2:
-            return <FontAwesomeIcon icon={faCheck} />; 
+            return <FontAwesomeIcon title = "Responded to message (interactive only)" icon={faCheck} />; 
         case -2:
-            return <FontAwesomeIcon icon={faTimes} />; 
+            return <FontAwesomeIcon title = "Error" icon={faTimes} />; 
         case 3:
-            return <FontAwesomeIcon icon={faMinus} />;
+            return <FontAwesomeIcon title = "Not responded (interactive only)" icon={faMinus} />;
         case -1:
-            return <FontAwesomeIcon icon={faQuestion} />;
+            return <FontAwesomeIcon title = "Not connected" icon={faQuestion} />;
+        default:
+            return <FontAwesomeIcon title = "Error" icon={faTimes} />;  
     }
 }
